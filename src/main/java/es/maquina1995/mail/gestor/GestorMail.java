e4ab39f8a4e737cfc1975de5131ac9e7c75ad843
MaQuiNa1995 <maquina1995@gmail.com>
2019-10-06 22:57:08 +0200

    Javadoc y mejora de nombres en properties

--- src/main/java/es/maquina1995/mail/gestor/GestorMail.java
+++ src/main/java/es/maquina1995/mail/gestor/GestorMail.java
@@ -1,7 +1,21 @@
 package es.maquina1995.mail.gestor;
 
+/**
+ * Clase encargada del envío de correos
+ * 
+ * @author MaQuiNa1995
+ *
+ */
 public interface GestorMail {
 
+	/**
+	 * Método usado para enviar correos
+	 * 
+	 * @param asunto
+	 *            String que contiene el asunto del correo
+	 * @param mensaje
+	 *            String que contiene el cuerpo del mensaje
+	 */
 	void envioCorreo(String asunto, String mensaje);
 
 }
--- src/main/java/es/maquina1995/mail/gestor/GestorMailImpl.java
+++ src/main/java/es/maquina1995/mail/gestor/GestorMailImpl.java
@@ -24,48 +24,66 @@ public class GestorMailImpl implements GestorMail {
 
 	private static final Logger LOGGER = Logger.getLogger(GestorMailImpl.class);
 
+	/**
+	 * Correo del remitente
+	 */
 	@Value("${mail.remitente}")
 	private String correoRemitente;
 
+	/**
+	 * Contraseña del remitente
+	 */
 	@Value("${mail.pass}")
 	private String contrasennaRemitente;
 
+	/**
+	 * Mail del destinatario
+	 */
 	@Value("${mail.destinatario}")
 	private String correoDestinatario;
 
+	/**
+	 * Bean de la configuracion del properties
+	 */
 	@Resource(name = "properties")
 	private Properties configuracionProperties;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see es.maquina1995.mail.gestor.GestorMail#envioCorreo(java.lang.String,
-	 * java.lang.String)
-	 */
 	public void envioCorreo(String asunto, String mensaje) {
 
+		// Iniciamos la sesion del correo
 		Session session = Session.getInstance(configuracionProperties,
 				autenticarCorreo());
 
 		try {
 
+			// Creamos el mensaje
 			Message message = new MimeMessage(session);
+			// Seteamos el remitente
 			message.setFrom(new InternetAddress(correoRemitente));
+			// Seteamos el destinatario o destinatarios
 			message.setRecipients(Message.RecipientType.TO,
 					InternetAddress.parse(correoDestinatario));
+			// Seteamos el asunto
 			message.setSubject(asunto);
 
+			// Creamos el cuerpo del mensaje
 			BodyPart texto = new MimeBodyPart();
+			// Seteamos el cuerpo del mensaje
 			texto.setText(mensaje);
 
-			MimeMultipart multiParte = new MimeMultipart();
+			// Por si quieres adjuntar ficheros...
 			// DataSource fuente = new FileDataSource(adjunto);
 			// texto.setDataHandler(new DataHandler(fuente));
 			// texto.setFileName(adjunto);
 
+			// Creamos las partes del correo
+			MimeMultipart multiParte = new MimeMultipart();
+			// Seteamos el cuerpo que contiene el mensaje
 			multiParte.addBodyPart(texto);
+			// Lo añadimos al correo
 			message.setContent(multiParte);
 
+			// Lo Enviamos
 			Transport.send(message);
 
 			LOGGER.info("Se ha mandado el correo");
@@ -75,6 +93,11 @@ public class GestorMailImpl implements GestorMail {
 		}
 	}
 
+	/**
+	 * Método usado para la autenticación del correo
+	 * 
+	 * @return javax.mail.Authenticator objeto para iniciar sesion en el correo
+	 */
 	private Authenticator autenticarCorreo() {
 
 		return new Authenticator() {
